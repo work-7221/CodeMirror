2bb11f85713b16d8ca848c288bd389f19e6eaa0d
work-7221 <work.rohankm7221@gmail.com>
2026-01-22 15:27:43 +0530

    Enabled Main.java to open up a OS native dialog box for folder selection; and getting its path for further implementations/modifications

--- Main.java
+++ Main.java
@@ -1,7 +1,33 @@
 package CodeMirror;
 
+import javax.swing.*;
+
+import java.io.File;
+import java.io.File.*;
+
+
 public class Main {
     public static void main(String[] args){
-        System.out.println("Hello World!");
+        System.out.println("Now we proceed to choose the folder where you have the file that you want to check for!!");
+        
+        JFileChooser chooser = new JFileChooser();
+        
+        // setting the dialog box to only allow directory selection!
+        
+        chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+
+        // setting the title of the file dialog box
+
+        chooser.setDialogTitle("Select A Folder");
+
+        // show the dialog box and get the result 
+        int result = chooser.showOpenDialog(null);
+
+        if (result == JFileChooser.APPROVE_OPTION) {
+            File selectedFolder = chooser.getSelectedFile();
+            System.out.println("Here's the selected folder: " + selectedFolder.getAbsolutePath());
+        } else {
+            System.out.println("File Selection Cancelled.");
+        }
     }
 }
