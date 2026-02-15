8514cd6f646360d5b9f6fad5a8bfd89eac79247d
work-7221 <work.rohankm7221@gmail.com>
2026-02-15 14:26:36 +0530

    Implemented N-gram based strcutring over normalized tokens so as to get better results when implmeneting similarity through jaccard similarity

---
+++ similarity_commands/N_gram.java
@@ -0,0 +1,74 @@
+package CodeMirror.similarity_commands;
+import java.util.*;
+
+public class N_gram {
+    public static List<List<String>> n_gram(int n, List<String> normalized_tokens) {
+        int j = n;
+        int length = normalized_tokens.size();
+        List<List<String>> list = new ArrayList<>();
+
+        // applying sliding window technique in order to get the N grams of each file
+        for (int i = 0; i < length; i++) {
+            
+            if (j >= length) {
+                break;
+            } else {
+                List<String> chunk = new ArrayList<>(normalized_tokens.subList(i, j));
+                list.add(chunk);  
+                j += 1;
+            }
+        }
+        return list;
+    }
+    
+    protected static double n_gram_score(List<String> file_a, List<String> file_b) {
+
+        // starting with jaccard similarity and making sets with n grams as it's tokens
+        Set<List<String>> A = new HashSet<List<String>>(); //FILE A
+        List<List<String>> A_L = n_gram(3, file_a);  
+        for (List<String> each: A_L) {
+            A.add(each);
+        }
+
+        Set<List<String>> B = new HashSet<List<String>>(); //FILE B
+        List<List<String>> B_L = n_gram(3, file_b);  
+        for (List<String> each: B_L) {
+            B.add(each);
+        }
+
+        // finding the intersection.
+        Set<List<String>> intersection = new HashSet<List<String>>(A); 
+        intersection.retainAll(B);
+
+        //finding the union.
+        Set<List<String>> union = new HashSet<List<String>>(A); 
+        union.addAll(B);
+
+        // accessing the number of elements in both union and intersection of the sets.
+        double n_intersection = intersection.size();
+        double n_union = union.size();
+
+        // applying jaccard similarity.
+        double score = (n_intersection/n_union)*100;
+
+        return score;
+    }
+
+    public static void hash_iterator(HashMap<String, List<String>> hash) { //this method accepts hashmap {<FILE_PATH> : <NORMALIZED_TOKENS>}
+        for(Map.Entry<String, List<String>> entry_a : hash.entrySet()) {
+            String path_a = entry_a.getKey();
+            List<String> content_a = hash.get(path_a);
+
+            for(Map.Entry<String, List<String>> entry_b : hash.entrySet()) {
+                String path_b = entry_b.getKey();
+                List<String> content_b = hash.get(path_b);
+                if (path_a.endsWith(".java") && path_b.endsWith(".java")){
+                    if (!(content_a == content_b)){ //ensuring that same file doesn't get compared with itself during the nested loop iteration.
+                        double score = n_gram_score(content_a, content_b);
+                        System.out.printf("| %-80s | %-80s | %-7.3f |%n",  path_a, path_b, score);
+                    }
+                }
+            }
+        }
+    }
+}
